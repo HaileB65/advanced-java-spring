b95aef0f111afa23c14a7bc289d7948bb06183d8
HaileB65 <brendon.haile@gmail.com>
2022-06-19 19:54:46 -0500

    Updated Spring core lab.

--- src/main/java/platform/codingnomads/co/corespring/lab/Cat.java
+++ src/main/java/platform/codingnomads/co/corespring/lab/Cat.java
@@ -1,4 +1,27 @@
 package platform.codingnomads.co.corespring.lab;
 
 public class Cat {
+    String name;
+    String bread;
+
+    public Cat(String name, String bread) {
+        this.name = name;
+        this.bread = bread;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
 }
--- src/main/java/platform/codingnomads/co/corespring/lab/Dog.java
+++ src/main/java/platform/codingnomads/co/corespring/lab/Dog.java
@@ -1,4 +1,27 @@
 package platform.codingnomads.co.corespring.lab;
 
 public class Dog {
+    String name;
+    String bread;
+
+    public Dog(String name, String bread) {
+        this.name = name;
+        this.bread = bread;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
 }
